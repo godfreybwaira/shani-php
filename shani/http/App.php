5160270989adce161d58021e955d907d82d29a81
Godfrey <qfadat@gmail.com>
2025-03-26 12:40:20 +0300

    Bug fixes

--- shani/http/App.php
+++ shani/http/App.php
@@ -280,28 +280,28 @@ namespace shani\http {
 
         /**
          * Render HTML document to user agent and close the HTTP connection.
-         * @param array $data Data object to be passed to a view file
+         * @param \JsonSerializable $dto Data object that will be passed to a view file
          * @param bool $useBuffer Set output buffer on so that output can be sent
          * in chunks without closing connection. If false, then connection will
          * be closed and no output can be sent.
          * @return void
          */
-        public function render(array $data = null, bool $useBuffer = false): void
+        public function render(\JsonSerializable $dto = null, bool $useBuffer = false): void
         {
-            $customData = $data ?? (new HttpMessageDto($this->response->status()))->jsonSerialize();
+            $customData = $dto ?? new HttpMessageDto($this->response->status());
             $type = $this->response->type();
             if ($type === DataConvertor::TYPE_HTML) {
                 ob_start();
-                $this->ui()->render($customData);
+                $this->ui()->render($customData->jsonSerialize());
                 $this->sendHtml(ob_get_clean(), $type);
             } else if ($type === DataConvertor::TYPE_SSE) {
                 ob_start();
-                $this->ui()->render($customData);
+                $this->ui()->render($customData->jsonSerialize());
                 $this->sendSse(ob_get_clean(), $type);
             } else if ($type === DataConvertor::TYPE_JS) {
-                $this->sendJsonp($customData, $type);
+                $this->sendJsonp($customData->jsonSerialize(), $type);
             } else {
-                $this->response->setBody(DataConvertor::convertTo($customData, $type), $type);
+                $this->response->setBody(DataConvertor::convertTo($customData->jsonSerialize(), $type), $type);
             }
             $this->send($useBuffer);
         }
