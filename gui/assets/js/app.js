dfab53a1c848c2613818c0576e6cb6c5d9dfb5f4
Godfrey <qfadat@gmail.com>
2025-10-13 20:40:36 +0300

    Bug fixes

--- gui/assets/js/app.js
+++ gui/assets/js/app.js
@@ -5,7 +5,7 @@ document.addEventListener('shani:init', () => {
         'shani-headers': 'x-request-mode:async'
     });
     Shani.action('ucase', (a) => a.emitter.value = a.emitter.value.toUpperCase());
-    Shani.action('formatter', (obj) => obj.emitter.innerHTML = obj.body);
+    Shani.action('formatter', (obj) => obj.emitter.innerHTML = obj.data.body);
     Shani.action('inf', (obj) => new FormData());
     Shani.action('test', (obj) => {
         return false; //try to return true...
